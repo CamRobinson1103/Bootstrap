b5cfbc0a60b7159793b5bc4d067b4f1f8c7ed793
Cam Robinson <s208065@students.aie.edu>
2021-10-12 14:03:59 -0500

    Created BuddhaWorld and in the process of adding a cube

---
+++ GraphicsProject/BuddahWorld.cpp
@@ -0,0 +1,31 @@
+#include "BuddahWorld.h"
+
+void BuddhaWorld::onStart()
+{
+	//Camera
+	m_camera = new PlayerCamera(45.0f, 0.001f, 1000.0f);
+	m_camera->getTransform()->setPosition(5.0f, 5.0f, 5.0f);
+	m_camera->getTransform()->rotate(-40.0f, -135.0f, 0.0f);
+	setCamera(m_camera);
+	add(m_camera);
+
+	//Light
+	m_light = new Light(
+		{ 1.0f, 1.0f , 1.0f },
+		{ 0.5f, 0.5f, 0.5f, 1.0f },
+		{ 12.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		);
+	add(m_light);
+	m_buddha = new OBJMesh();
+	m_buddha->load("Buddha.obj");
+	add(m_buddha);
+}
+
+
+void BuddhaWorld::onEnd()
+{
+	destroy(m_camera);
+	destroy(m_light);
+	destroy(m_buddha);
+}
