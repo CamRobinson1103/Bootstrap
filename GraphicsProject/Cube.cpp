d9a1d81e49dd4b72bf79594ab2946acaf7bdcbdb
Cam Robinson <s208065@students.aie.edu>
2021-10-14 16:55:34 -0500

    Deleted empty spaces

--- GraphicsProject/Cube.cpp
+++ GraphicsProject/Cube.cpp
@@ -12,48 +12,35 @@ Cube::Vertex* Cube::generateVertices(unsigned int& vertexCount, unsigned int& tr
 	initializeCorner(vertices, vertexCount, 0, { 1.0f,  1.0f, 1.0f,});  
 	initializeCorner(vertices, vertexCount, 1, { -1.0f, 1.0f, 1.0f,});  
 	initializeCorner(vertices, vertexCount, 2, { -1.0f, -1.0f,	1.0f,});  
-
 	initializeCorner(vertices, vertexCount, 3, { -1.0f,	-1.0f, 1.0f, });  
 	initializeCorner(vertices, vertexCount, 4, { 1.0f,	-1.0f,	 1.0f, });  
 	initializeCorner(vertices, vertexCount, 5, { 1.0f,	 1.0f,	1.0f, }); 
-
 	initializeCorner(vertices, vertexCount, 6, { 1.0f, 1.0f, 1.0f, });  
 	initializeCorner(vertices, vertexCount, 7, { 1.0f, -1.0f, 1.0f, });  
 	initializeCorner(vertices, vertexCount, 8, { 1.0f, -1.0f, -1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 9, { 1.0f, -1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 10, { 1.0f,	1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 11, { 1.0f,	1.0f, 1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 12, { 1.0f,	1.0f, 1.0f, });  
 	initializeCorner(vertices, vertexCount, 13, { 1.0f,	1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 14, { -1.0f, 1.0f, -1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 15, { -1.0f, 1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 17, { 1.0f,	 1.0f,	1.0f, });  
-
-
 	initializeCorner(vertices, vertexCount, 18, { -1.0f, -1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 19, { -1.0f, 1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 20, { 1.0f,	1.0f, -1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 21, { 1.0f,	1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 22, { 1.0f,	-1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 23, { -1.0f, -1.0f, -1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 24, { -1.0f, -1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 25, { 1.0f, -1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 26, { 1.0f, -1.0f, 1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 27, { 1.0f,	-1.0f, 1.0f, }); 
 	initializeCorner(vertices, vertexCount, 28, { -1.0f, -1.0f,	 1.0f, });  
 	initializeCorner(vertices, vertexCount, 29, { -1.0f, -1.0f, -1.0f, });  
-
-
 	initializeCorner(vertices, vertexCount, 30, { -1.0f, -1.0f, -1.0f, });  
 	initializeCorner(vertices, vertexCount, 31, { -1.0f, -1.0f,	 1.0f, });  
 	initializeCorner(vertices, vertexCount, 32, { -1.0f, 1.0f,	1.0f, });  
-
 	initializeCorner(vertices, vertexCount, 33, { -1.0f, 1.0f, 1.0f, });  
 	initializeCorner(vertices, vertexCount, 34, { -1.0f, 1.0f,	-1.0f, });  
 	initializeCorner(vertices, vertexCount, 35, { -1.0f, -1.0f,	-1.0f, });  
